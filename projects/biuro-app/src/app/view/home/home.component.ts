fbdc9afc95d9b7436eb77ee3b82fc4f426f80a04
theMooja <the.mooja@gmail.com>
2024-03-27 15:01:58 +0100

    test button

--- projects/biuro-app/src/app/view/home/home.component.ts
+++ projects/biuro-app/src/app/view/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  testVal: String = "tu";
 }
