6f05b34251bfe73f055691ffcb6318c78073bacf
theMooja <the.mooja@gmail.com>
2024-04-17 13:20:49 +0200

    move User schema to its file

--- projects/electron/src/api/db.ts
+++ projects/electron/src/api/db.ts
@@ -1,22 +1,7 @@
-import { Schema, model } from 'mongoose';
+import Clients from './Users';
 
-interface IUser {
-    name: string;
-}
 
-const userSchema = new Schema<IUser>({
-    name: { type: String, required: true }
-});
-
-const User = model<IUser>('User', userSchema);
 
 export default {
-    testData(name: string): string {
-        const user = new User({
-            name: name
-        });
-        user.save();
-
-        return 'dbTestData';
-    }
+    ...Clients
 }
