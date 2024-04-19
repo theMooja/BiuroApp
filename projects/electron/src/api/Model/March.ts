9419281bbfb60b810f2873d508dfb0535765c9cb
theMooja <the.mooja@gmail.com>
2024-04-19 14:05:11 +0200

    march schema

--- projects/electron/src/api/Model/March.ts
+++ projects/electron/src/api/Model/March.ts
@@ -1,21 +1,33 @@
 import { Schema, model } from 'mongoose';
 
-interface IMarch {
-}
 interface IMarchStep {
 
 }
+interface IMarch {
+    name: string;
+    children: [IMarchStep]
+}
 
-const marchSchema = new Schema<IMarch>({
-    name: { type: String, required: true }
-});
-const March = model<IMarch>('March', marchSchema);
 
 const stepSchema = new Schema<IMarchStep>({
-
+    value: { type: Number },
 });
 const MarchStep = model<IMarchStep>('MarchStep', stepSchema);
 
+const marchSchema = new Schema<IMarch>({
+    name: { type: String, required: true },
+    children: [stepSchema]
+});
+const March = model<IMarch>('March', marchSchema);
+
+
+
 export default {
-    
+    create(name: string, steps: IMarchStep[]) {
+        const march = new March({
+            name: name,
+            children: steps
+        });
+        march.save();
+    }
 }
