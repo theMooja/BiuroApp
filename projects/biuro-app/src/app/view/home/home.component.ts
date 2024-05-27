4259e0de8751eaa4b8a0eea54df8fc3a94106f7e
theMooja <the.mooja@gmail.com>
2024-05-27 11:50:51 +0200

    expandable rows on main view

--- projects/biuro-app/src/app/view/home/home.component.ts
+++ projects/biuro-app/src/app/view/home/home.component.ts
@@ -2,16 +2,26 @@ import { Component } from '@angular/core';
 import { ClientDataService } from '../../service/client-data.service';
 import { IClient } from '../../../../../electron/src/interfaces';
 import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),],
 })
 export class HomeComponent {
   clients: IClient[];
+  expandedElement: IClient | null = null;
 
   constructor(private dataService: ClientDataService) {
     this.clients = [];
@@ -19,6 +29,5 @@ export class HomeComponent {
 
   async ngOnInit() {
     this.clients = await this.dataService.getClientsMonthly(2024, 1);
-    console.log('got', this.clients);
   }
 }
