5d86dd9346cc6f90d4830ef826448bc6d69eb747
theMooja <the.mooja@gmail.com>
2025-02-05 10:43:18 +0100

    info view cards

--- projects/biuro-app/src/app/view/info/info.component.ts
+++ projects/biuro-app/src/app/view/info/info.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
