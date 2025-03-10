59a80a1b1af2e3ef727c853a8f449525eb75d44d
theMooja <the.mooja@gmail.com>
2025-03-10 14:18:17 +0100

    invoice sums disaplyed

--- projects/biuro-app/src/app/view/home/invoice-column/invoice-column.component.ts
+++ projects/biuro-app/src/app/view/home/invoice-column/invoice-column.component.ts
@@ -7,26 +7,45 @@ import { MatButtonModule } from '@angular/material/button';
 import { InvoiceOverlayComponent } from '../invoice-overlay/invoice-overlay.component';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 export const DATA_INJECTION_TOKEN = new InjectionToken<{ monthly: IMonthlyEntity, invoice: IInvoiceEntity, overlayRef: OverlayRef }>('DATA_INJECTION_TOKEN');
 
 @Component({
   selector: 'invoice-column',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatListModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatListModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './invoice-column.component.html',
   styleUrl: './invoice-column.component.scss'
 })
 export class InvoiceColumnComponent {
   @Input() monthly!: IMonthlyEntity;
 
+  sumCat1 = this.getSumInvoice('księgowość');
+  sumCat2 = this.getSumInvoice('kadry');
+
   constructor(
     private overlay: Overlay
   ) {
   }
 
+  ngOnInit() {
+    this.sumCat1 = this.getSumInvoice('księgowość');
+    this.sumCat2 = this.getSumInvoice('kadry');
+
+  }
+
   get lines() {
     return this.monthly.invoices[0]?.lines;
+  }
+
+  getSumInvoice(category: string) {
+    if (!this.monthly || !this.monthly.invoices[0] || !this.lines) return 0;
+    return this.lines
+      .filter(line => line.category === category)
+      .reduce((acc, line) => acc + (line.price * line.qtty), 0);
   }
 
   onEdit() {
@@ -55,5 +74,10 @@ export class InvoiceColumnComponent {
     const overlayPortal = new ComponentPortal(InvoiceOverlayComponent, null, data);
     overlayRef.attach(overlayPortal);
     overlayRef.backdropClick().subscribe(() => overlayRef.dispose());
+    overlayRef.detachments().subscribe(() => {
+
+      this.sumCat1 = this.getSumInvoice('księgowość');
+      this.sumCat2 = this.getSumInvoice('kadry');
+    });
   }
 }
