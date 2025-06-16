04043440e1fcc6cdf5b6f1ab515f7bd769dd01bf
theMooja <the.mooja@gmail.com>
2025-06-16 14:23:38 +0200

    pick note user

--- projects/biuro-app/src/app/view/home/notes-column/notes.component.ts
+++ projects/biuro-app/src/app/view/home/notes-column/notes.component.ts
@@ -1,5 +1,5 @@
 import { afterNextRender, Component, EventEmitter, inject, Injector, Input, Output, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
-import { IMonthlyEntity, INoteEntity } from '../../../../../../electron/src/interfaces';
+import { IMonthlyEntity, INoteEntity, IUserEntity } from '../../../../../../electron/src/interfaces';
 import { MatButtonModule } from '@angular/material/button';
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
@@ -14,12 +14,14 @@ import { UserDataService } from '../../../service/user-data.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { addMonths } from 'date-fns';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'notes-column',
   standalone: true,
   imports: [MatButtonModule, FormsModule, MatFormFieldModule, MatInputModule, TextFieldModule, CommonModule, MatIconModule, MatCardModule,
-    MatDatepickerModule,
+    MatDatepickerModule, MatSelectModule
   ],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
@@ -31,8 +33,10 @@ export class NotesComponent {
   private _injector = inject(Injector);
   @ViewChild('autosize') autosize!: CdkTextareaAutosize;
   currentNote: INoteEntity;
+  private userDataService: UserDataService = inject(UserDataService);
+  userId: number = 0;
 
-  constructor(private monthlyDataService: MonthlyDataService, private userDataService: UserDataService,
+  constructor(private monthlyDataService: MonthlyDataService,
     private overlay: Overlay, private vcr: ViewContainerRef) { }
 
   get notes() {
@@ -48,6 +52,12 @@ export class NotesComponent {
     this.currentNote.text = val
   }
 
+  get users(): IUser[] {
+    return this.userDataService.data.map(user => ({
+      id: user.id,
+      name: user.name
+    }));
+  }
 
   async onDelete() {
     if (this.currentNote.deleteing) {
@@ -65,12 +75,16 @@ export class NotesComponent {
   }
 
   async onSave() {
+    this.currentNote.user = this.getUserEntity();
     let note = await this.monthlyDataService.updateNote(this.currentNote);
     this.currentNote.id = note.id;
     this.isEdit = false;
     this.hideTooltip();
+  }
 
-    //this.refresh.emit(this.monthly.id);
+  getUserEntity(): IUserEntity | undefined {
+    if (this.userId === 0) return undefined;
+    return this.userDataService.data.find(user => user.id === this.userId);
   }
 
   onEdit() {
@@ -87,21 +101,19 @@ export class NotesComponent {
     this.notes.push(note);
   }
 
-  onPinUser() {
-    if (this.currentNote.user) {
-      delete this.currentNote.user;
-      return;
-    }
-    this.currentNote.user = this.userDataService.user;
-  }
-
   onPersists() {
     this.currentNote.persists = !this.currentNote.persists;
   }
 
   showTooltip(tooltipTemplate: TemplateRef<any>, note: INoteEntity, event: MouseEvent) {
     if (this.isEdit) return;
+
     this.currentNote = note;
+    if (this.currentNote?.user) {
+      this.userId = this.currentNote.user.id;
+    } else {
+      this.userId = 0;
+    }
 
     if (!this.overlayRef) {
       const positionStrategy = this.overlay
@@ -154,4 +166,9 @@ export class NotesComponent {
   onClearDueDate() {
     this.currentNote.dueDate = undefined;
   }
+}
+
+interface IUser {
+  id: number;
+  name: string;
 }
