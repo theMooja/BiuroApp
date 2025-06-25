f8371975e8e01f0b2d6f2eb7282433f9e4bb8c33
theMooja <the.mooja@gmail.com>
2025-06-25 14:27:10 +0200

    zalando nieobecnosci

--- projects/biuro-app/src/app/view/tools/zalando/zalando.component.ts
+++ projects/biuro-app/src/app/view/tools/zalando/zalando.component.ts
@@ -1,14 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { read } from 'fs';
 import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-zalando',
   standalone: true,
-  imports: [MatBadgeModule, MatInputModule, MatButtonModule],
+  imports: [MatBadgeModule, MatInputModule, MatButtonModule, CommonModule],
   templateUrl: './zalando.component.html',
   styleUrl: './zalando.component.scss'
 })
@@ -17,9 +17,12 @@ export class ZalandoComponent {
   absencesFile: File | null = null;
   absences: {
     employee: string,
+    surname: string,
+    name: string,
     absenceType: string,
     startDate: string,
     endDate: string,
+    matched?: boolean
   }[] = [];
 
   employeesFile: File | null = null;
@@ -27,36 +30,75 @@ export class ZalandoComponent {
     name: string,
     surname: string,
     kod: string,
-    pesel: string,
   }[] = [];
 
-  onLoadEmployees(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      this.employeesFile = input.files[0];
-    } else return;
+  definitionsFile: File | null = null;
+  definitions: {
+    name: string,
+    reason: string,
+    mapFrom: string
+  }[] = [];
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      console.log('File loaded:', this.employeesFile);
-      const data = new Uint8Array(reader.result as ArrayBuffer);
-      const workbook = XLSX.read(data, { type: 'array' });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      const sheetData = XLSX.utils.sheet_to_json(worksheet, { header: 0, blankrows: false });
-      console.log('Sheet data:', sheetData);
+  results: {
+    kod: string,
+    def: string,
+    reason: string,
+    from: string,
+    to: string,
+  }[] = [];
 
-      this.employees = sheetData.map((row: any) => ({
-        name: row["Imie"],
-        surname: row["Nazwisko"],
-        kod: row["Kod"],
-        pesel: row["Workers.Info.Historia.PESEL"],
-      }));
-      console.log('Parsed employees:', this.employees);
-    }
+  onMatch() {
+    this.results = [];
+    this.absences.forEach(absence => {
+      let employee: typeof this.employees[0] | undefined = undefined;
+      const employees = this.employees.filter(emp => emp.surname === absence.surname);
+      if (employees.length === 1) {
+        employee = employees[0];
+      } else if (employees.length > 1) {
+        employee = employees.find(emp => absence.name.includes(emp.name));
+      } else {
+        console.warn(`No employee found for absence: ${absence.employee}`);
+      }
+      if (!employee) {
+        console.warn(`No matching employee for absence: ${absence.employee}`);
+        return;
+      }
 
-    reader.readAsArrayBuffer(this.employeesFile as File);
+      const definition = this.definitions.find(def => def.mapFrom === absence.absenceType);
+      if (!definition) {
+        console.warn(`No definition found for absence type: ${absence.absenceType}`);
+        return;
+      }
+      absence.matched = true;
+
+      this.results.push({
+        kod: employee.kod,
+        def: definition.name,
+        reason: definition.reason,
+        from: absence.startDate,
+        to: absence.endDate
+      });
+    });
+    console.log('Matching results:', this.results);
   }
+
+  async onExport() {
+    const aoa = this.results.map(result => [
+      'NieobecnośćPracownika',
+      'PracownikFirmy',
+      result.kod,
+      result.def,
+      result.reason,
+      result.from,
+      result.to
+    ]);
+    aoa.unshift(['Class', 'Pracownik:Class', 'Pracownik:Kod', 'Definicja:nazwa', 'Zwolnienie.Przyczyna', 'Okres.Od', 'Okres.Do']);
+
+    const worksheet = XLSX.utils.aoa_to_sheet(aoa);
+    const tsv = XLSX.utils.sheet_to_txt(worksheet, { FS: '\t' });
+    await navigator.clipboard.writeText(tsv);
+  }
+
 
   onLoadAbsences(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -74,11 +116,16 @@ export class ZalandoComponent {
       const sheetData = XLSX.utils.sheet_to_json(worksheet, { header: 1, range: 10, blankrows: false });
       console.log('Sheet data:', sheetData);
 
+      const surnameRegex = /^[\p{L}\p{M}]+/u;
+      const nameRegex = /^(?:[\p{L}\p{M}]+\s+)(.*?)(?=\s*\()/u;
+
       this.absences = sheetData.map((row: any) => ({
         employee: row[0],
         absenceType: row[2],
         startDate: row[3],
         endDate: row[4],
+        surname: (row[0].match(surnameRegex)?.[0] || '').toUpperCase(),
+        name: (row[0].match(nameRegex)?.[1] || '').toUpperCase(),
       }));
       console.log('Parsed absences:', this.absences);
     };
@@ -86,7 +133,44 @@ export class ZalandoComponent {
     reader.readAsArrayBuffer(this.absencesFile as File);
   }
 
-  onLoadDefinitions() {
-    // Logic to load definitions goes here
+  onLoadDefinitions(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.definitionsFile = input.files[0];
+    } else return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      console.log('File loaded:', this.definitionsFile);
+      const data = new Uint8Array(reader.result as ArrayBuffer);
+      const workbook = XLSX.read(data, { type: 'array' });
+      this.parseEmployees(workbook.Sheets['pracownicy']);
+      console.log('Parsed employees:', this.employees);
+      this.parseDefinitions(workbook.Sheets['nieobecnosci']);
+      console.log('Parsed definitions:', this.definitions);
+    };
+
+    reader.readAsArrayBuffer(this.definitionsFile as File);
+  }
+
+  parseEmployees(sheet: XLSX.WorkSheet) {
+    const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 0, blankrows: false });
+
+    this.employees = sheetData.map((row: any) => ({
+      name: row["Imie"].toUpperCase(),
+      surname: row["Nazwisko"].toUpperCase(),
+      kod: row["Kod"]
+    }));
+
+  }
+
+  parseDefinitions(sheet: XLSX.WorkSheet) {
+    const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 0, blankrows: false });
+    this.definitions = sheetData.map((row: any) => ({
+      name: row["Definicja:Nazwa"],
+      reason: row["Zwolnienie.Przyczyna"],
+      mapFrom: row["zalando"],
+    }));
+
   }
 }
